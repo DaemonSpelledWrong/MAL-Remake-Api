af99f80216b078b810e5bb4e42756af801f8a3e9
Damon Self <damongself@gmail.com>
2020-08-06 09:14:44 -0400

    Updates routes, cors, and controller for Anime resources
    This PR includes the following changes:
    - cors has been configured to accept requests from any resources
    - The cors Gem has been uncommented in the Gemfile
    - There is now seeded anime data to use for testing
    - The index and show routes have been opened for the Anime controller
    
    Tests coming soon.

---
+++ app/controllers/animes_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class AnimesController < ApplicationController
+  def index
+    @animes = Anime.all
+
+    render json: @animes
+  end
+
+  def show
+    @anime = Anime.find(params[:id])
+
+    render json: @anime
+  end
+end
