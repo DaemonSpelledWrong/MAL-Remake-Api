2c12ac0d5b3934a1632d0fa118c31ffc5681917d
Damon Self <damongself@gmail.com>
2020-08-16 09:41:01 -0500

    Implements Manga controller tests and fixtures
    Previously there were no fixtures for the controller to draw upon
    when testing. Now there are two unique manga instances that are
    used in the newly written mangecontroller action tests for index
    and show.

--- test/controllers/mangas_controller_test.rb
+++ test/controllers/mangas_controller_test.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class MangasControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get index' do
+    get mangas_url
+    assert_response :success
+  end
+
+  test 'should show anime' do
+    get mangas_url(Manga.first)
+    assert_response :success
+  end
 end
